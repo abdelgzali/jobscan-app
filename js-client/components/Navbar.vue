2128b6143b6b2d12e20d247c8645d8300d024478
abd <abdelgzali@gmail.com>
2020-12-14 20:18:50 -0800

    submodule issue

---
+++ js-client/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav id="nav">
+
+  </nav>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss">
+
+nav#nav {
+  height: 65px;
+  width: 100%;
+  padding: 1rem 2rem;
+  background-color: $primary-blue;
+  color: #fff;
+}
+</style>
---
+++ js-client/components/dropdown.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in listItems" :key="index">
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['initialList'],
+  data: function () {
+    return {
+      listItems: this.initialList
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
